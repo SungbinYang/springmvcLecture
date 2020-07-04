8a65c2f647126771a11e66c7ec124f05995865f2
Sungbin <ysb05222@naver.com>
2020-07-04 19:19:15 +0900

    Update UserController.java

--- src/main/java/kr/ac/mjc/sungbin/springmvc/user/UserController.java
+++ src/main/java/kr/ac/mjc/sungbin/springmvc/user/UserController.java
@@ -1,8 +1,12 @@
 package kr.ac.mjc.sungbin.springmvc.user;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
@@ -15,6 +19,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -30,8 +35,7 @@ public class UserController {
 	 * 다른 매핑이 안된 URI는 모두 여기에 매핑된다. 기본 뷰로 forward
 	 */
 	@GetMapping("/**")
-	public void mapDefault(HttpServletRequest request) {
-		logger.debug("Default mapping : {}", request.getPathInfo());
+	public void mapDefault() {
 	}
 
 	/**
@@ -56,7 +60,6 @@ public class UserController {
 		model.addFlashAttribute("user", user);
 		try {
 			userDao.addUser(user);
-			// 회원가입 성공
 			return "redirect:/app/user/joinComplete";
 		} catch (DuplicateKeyException e) { // 이메일 중복으로 가입실패
 			return "redirect:/app/user/joinForm?mode=ERROR";
@@ -67,16 +70,22 @@ public class UserController {
 	 * 로그인
 	 */
 	@PostMapping("/login")
-	public String login(String email, String password, HttpSession session,
-			RedirectAttributes model) {
+	public void login(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String returnUrl = request.getParameter("returnUrl");
 		try {
 			User user = userDao.getUserByEmailAndPassword(email, password);
 			// 로그인 성공
-			session.setAttribute("user", user);
-			return "redirect:/app/user/s/userInfo?mode=LOGIN";
+			request.getSession().setAttribute("user", user);
+			response.sendRedirect(returnUrl);
 		} catch (EmptyResultDataAccessException e) { // 로그인 실패
-			model.addFlashAttribute("email", email);
-			return "redirect:/app/user/loginForm?mode=ERROR";
+			String loginUrl = String.format(
+					"%s/app/user/loginForm?mode=FAILURE&email=%s&returnUrl=%s",
+					request.getContextPath(), email,
+					URLEncoder.encode(returnUrl, Charset.defaultCharset()));
+			response.sendRedirect(loginUrl);
 		}
 	}
 
@@ -87,5 +96,70 @@ public class UserController {
 	public String logout(HttpSession session) {
 		session.invalidate();
 		return "redirect:/";
+	}
+
+	/**
+	 * 회원정보 수정<br>
+	 * 
+	 * @param user        Command object. 폼에서 submit한 사용자 정보
+	 * @param sessionUser 세션의 로그인한 유저 정보
+	 */
+	@PostMapping("/user/s/updateUser")
+	public String updateUser(User user,
+			@SessionAttribute("user") User sessionUser,
+			RedirectAttributes model) {
+		user.setId(sessionUser.getId());
+		try {
+			userDao.updateUser(user);
+			// 개인정보 수정에 성공하면 세션값을 변경
+			sessionUser.setEmail(user.getEmail());
+			sessionUser.setName(user.getName());
+			return "redirect:/app/user/s/userInfo?mode=UPDATE";
+		} catch (DuplicateKeyException e) {
+			// 이메일 중복으로 수정에 실패할 경우
+			model.addFlashAttribute("user", user);
+			return "redirect:/app/user/s/userEdit?mode=ERROR";
+		}
+	}
+
+	/**
+	 * 비밀번호 수정
+	 */
+	@PostMapping("/user/s/updatePassword")
+	public String updatePassword(String password, String newPassword,
+			@SessionAttribute("user") User sessionUser) {
+		int updatedRows = userDao.updatePassword(sessionUser.getId(), password,
+				newPassword);
+		if (updatedRows == 1) { // 비밀번호 변경 성공
+			return "redirect:/app/user/s/userInfo?mode=PASSWORD";
+		} else { // 현재 비밀번호 틀림
+			return "redirect:/app/user/s/passwordEdit?mode=ERROR";
+		}
+	}
+
+	/**
+	 * 회원탈퇴
+	 */
+	@GetMapping("/user/s/dropUser")
+	public String dropUser(HttpSession session) {
+		User sessionUser = (User) session.getAttribute("user");
+		userDao.deleteUser(sessionUser.getId());
+		logger.info("회원탈퇴 했습니다. {}", sessionUser);
+		return logout(session);
+	}
+
+	/**
+	 * 회원정보 보기
+	 */
+	@GetMapping("/user/userInfo")
+	public void userInfo(String id, Model model) {
+		User user = null;
+		try {
+			user = userDao.getUserById(id);
+		} catch (EmptyResultDataAccessException e) {
+			user = new User();
+			user.setId("등록되지 않은 사용자");
+		}
+		model.addAttribute("user", user);
 	}
 }
